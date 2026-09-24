9c3003ca292fa2c9f6508ab4c9283d79b8ae8224
prze8969 <shewalepranav66@gmail.com>
2026-09-24 23:28:56 +0530

    stack things

--- Stackk/Stack.java
+++ Stackk/Stack.java
@@ -1,8 +1,6 @@
 class Stack{
-
-
-
 	int[] arr;
+	int capacity;
 	int top;
 	Stack(int size){
 		arr = new int[size];
@@ -13,9 +11,29 @@ class Stack{
 	}
 
 	boolean isFull(){
+		return(top == capacity -1);
 		}
-
 	void pushToStack(int data){
-		
+		if(isFull()){
+			System.out.println("The stack is full please empty it before using");
+			return;
+		}
+		arr[++top] = data;
 	}
+
+	int popFromStack(){
+		if(isEmpty()){
+			System.out.println("The stack is Empty please fill it");
+			return -1;
+		}
+		return arr[top--];
+	}
+
+	int peek(){
+		if(isEmpty()){
+			System.out.println("Nothing here in the stack");
+			return -1;
+		}
+		return arr[top];
+	}	
 }
