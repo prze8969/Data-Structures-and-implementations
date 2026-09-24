270e23c486875bdab2764a6c45abeecc78d2d16e
prze8969 <shewalepranav66@gmail.com>
2026-09-24 20:28:57 +0530

    added stackk

---
+++ Stackk/Stack.java
@@ -0,0 +1,21 @@
+class Stack{
+
+
+
+	int[] arr;
+	int top;
+	Stack(int size){
+		arr = new int[size];
+		top = -1;
+	}
+	boolean isEmpty(){
+		return(top == empty);
+	}
+
+	boolean isFull(){
+		}
+
+	void pushToStack(int data){
+		
+	}
+}
