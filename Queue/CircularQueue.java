d6f68aadb40427e6932984a721257ccd9605486e
prze8969 <shewalepranav66@gmail.com>
2026-09-25 18:59:00 +0530

    Added circularQueue and queuenotes

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -10,16 +10,71 @@ class CircularQueue{
     rear = -1;
   }
   boolean isFull(){
-    return((rear +1)%capacity == front);
+    return((rear +1)%size_of_array == front);
   }
 
   boolean isEmpty(){
     return (front == -1);
   }
   void enqueue(int value){
+    if(isFull()){
+      System.out.println("The queue is full");
+      return;
+    }
     if( front == -1 && rear == -1){
       //the queue isnt initialized
-       
+      front = 0 ;
+      rear = 0;
      }
+     arr[rear] = value;
+     rear = (rear+1)%size_of_array;
+  }
+
+  int dequeue(){
+
+    //if the queue is empty, then dont do anything
+    if(isEmpty()){
+      System.out.println("Empty queue");
+      return -1;
+      
+          }
+          
+    int resVal = arr[front];
+    front = (front+1)%size_of_array;
+
+    
+    // if the queue in future becomes empty by front == rear then just
+    // reset the queue to the start
+    // f = 0 r = 1 then queue  = [3,_,_,_,_]
+    // dequeue : f = 1 r = 1 queue = [_,_,_,_,_]
+    // since f = r = 1 hence it means queue is empty
+    if(front ==  rear){
+      front = -1;
+      rear = -1;
+    }
+
+    return resVal;
+  }
+
+  int peek(){
+    if(isEmpty()){
+      System.out.println("The queue is empty");
+      return -1;
+    }
+    return arr[front];
+  }
+
+  void display(){
+    
+    if(isEmpty()){
+      System.out.println("The queue is empty");
+      return;
+    }
+    int n = size_of_array;
+    int i = front;
+    while(i!= rear){
+      System.out.println(arr[i]);
+      i = (i+1)%n;
+    }
   }
 }
